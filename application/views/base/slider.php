6e4f6faf241f7d577e5e24e8220c33eda35aeed2
JuanDelgadillo <jsebastian.delgadillo@gmail.com>
2019-02-01 00:27:26 -0300

    cambiocolor menu y agregocaption a imagenes slider

--- application/views/base/slider.php
+++ application/views/base/slider.php
@@ -7,12 +7,24 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src="<?php echo base_url(); ?>assets/img/carrusel/1.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Uma prueba</h2>
+        <p>ja ja ja</p>
+      </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="<?php echo base_url(); ?>assets/img/carrusel/1.jpg" alt="Second slide">
+       <div class="carousel-caption d-none d-md-block">
+        <h5>...</h5>
+        <p>...</p>
+      </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="<?php echo base_url(); ?>assets/img/carrusel/1.jpg" alt="Third slide">
+       <div class="carousel-caption d-none d-md-block">
+        <h5>...</h5>
+        <p>...</p>
+      </div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
